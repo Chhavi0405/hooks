354e4c35b8405609805b7e40a3cf2096d2f6d007
Chhavi Choudhary <chhavi.2023@tecorb.co>
2023-10-26 10:46:36 +0530

    remove consoles and styling

--- app/page.tsx
+++ app/page.tsx
@@ -22,20 +22,20 @@ export default function Home() {
     useConversion();
   const { sortDateAsc, sortDateDesc } = UseDateSorting();
 
-  const [input, setInput] = useState<string | number>();
-  const [isDay, setIsDay] = useState<string | number>();
-  const [isFullYearFormat, setIsFullYearFormat] = useState<string | number>();
-  const [inputDay, setInputDay] = useState<string | number>();
-  const [isMonth, setIsMonth] = useState<string | number | Date>();
-  const [isFullTime, setIsFullTime] = useState<string | number | Date>();
-  const [isTime, setIsTime] = useState<number | Date | string>();
-  const [isMonthFull, setIsMonthFull] = useState<string | number | Date>();
-  const [is12hrs, setIs12Hrs] = useState<number | string | Date>();
-  const [is24hrs, setIs24Hrs] = useState<number | string | Date>();
-  const [isTimeDate, setisTimeDate] = useState<number | string | Date>();
+  const [input, setInput] = useState<string | number>('');
+  const [isDay, setIsDay] = useState<string | number>('');
+  const [isFullYearFormat, setIsFullYearFormat] = useState<string | number>('');
+  const [inputDay, setInputDay] = useState<string | number>('');
+  const [isMonth, setIsMonth] = useState<string | number | Date>('');
+  const [isFullTime, setIsFullTime] = useState<string | number | Date>('');
+  const [isTime, setIsTime] = useState<number | Date | string>('');
+  const [isMonthFull, setIsMonthFull] = useState<string | number | Date>('');
+  const [is12hrs, setIs12Hrs] = useState<number | string | Date>('');
+  const [is24hrs, setIs24Hrs] = useState<number | string | Date>('');
+  const [isTimeDate, setisTimeDate] = useState<number | string | Date>('');
   const [isAsc, setIsAsc] = useState<number[] | Date[] | undefined>([]);
   const [isDesc, setIsDesc] = useState<number[] | Date[] | undefined>([]);
-
+  const [isFormatDate2,setIsFormatDate2] = useState<string|number>('')
   const dates = [
     new Date(1995, 6, 2),
     new Date(1997, 6, 2),
@@ -47,17 +47,14 @@ export default function Home() {
     setIsAsc(sortDateAsc(dates));
   };
 
-  console.log(isAsc, "ascending");
-  const ascDates = sortDateAsc(dates);
-  //  console.log(ascDates,"qwer")
+const handleFormatDate2 =()=>{
+  setIsFormatDate2(getddMMyy(getDate()))
+}
 
   const handleDesc = () => {
     setIsDesc(sortDateDesc(dates));
   };
 
-  console.log(isDesc, "descending");
-  // const descendDate = sortDateDesc(dates2)
-  // console.log(descendDate,"descend")
 
   const handleFormatdate = () => {
     setInput(getYear(getDate()));
@@ -96,85 +93,98 @@ export default function Home() {
   };
 
   return (
-    <> 
+    <>
 
-    <div className="px-4 py-4 ">
-      <h2 className="text-xl text-center italic font-extrabold text-red-950 mb-4">Custom Hooks</h2>
-    
-      <h2 className="text-xl font-medium mb-2">Date Formats</h2>
-      <button className=" border-2 mb-3  space-x-4" onClick={() => handleGetDay()}>format Date</button> 
-      
-      <p className=" space-x-4 text-pink-950">{isDay}</p>
-     
-      <button className=" border-2 mb-3" onClick={() => getddMMyy(getDate())}>
-        format Date2 in ddMMyy
-      </button>
-      <br />
-      <button className=" border-2 mb-3" onClick={() => handleddMMyy()}>
-        format Date3 in yyyyMMdd
-      </button>{" "}
-      <p className=" space-x-4 text-pink-950">{isFullYearFormat}</p>
-      
-      <br />
-      <button className=" border-2 mb-3" onClick={() => handleDay()}>getday</button>
-      <p className=" space-x-4 text-pink-950">{inputDay}</p>
-      
-      <br />
-      <button className=" border-2 mb-3" onClick={() => handleMonth()}>getMonth</button>
-      <p className=" space-x-4 text-pink-950">{isMonth}</p> 
-      <br />
-      <button className=" border-2 mb-3" onClick={() => handleFormatdate()}>getYear</button>
-      <p className=" space-x-4 text-pink-950">{input}</p>
-      <br />
-      <button  className=" border-2 mb-3" onClick={() => handleGetTime()}>get Time In HH:MM</button>
-      <p className=" space-x-4 text-pink-950">{isTime}</p>
-      <br />
-      <button className=" border-2 mb-3" onClick={() => handleFullTime()}>time in HH:MM:SS</button>
-      <p className=" space-x-4 text-pink-950">{isFullTime}</p>
-      <br />
-      <button className=" border-2 mb-3" onClick={() => handleFullMonth()}>get full month</button>
-      <p className=" space-x-4 text-pink-950">{isMonthFull}</p> 
-     
-      </div>
+      <div className="px-4 py-4 ">
+        <h2 className="text-xl text-center italic font-extrabold text-red-950 mb-4">Custom Hooks</h2>
 
-      <div className="px-4">
-      <h2 className="text-xl font-medium mb-2">Time Conversion</h2>
-  
-      <button className=" border-2 mb-3"
-        onClick={() => {
-          handleTime();
-        }}
-      >
-        in 12 hrs
-      </button>
-      &nbsp;{is12hrs}
-      <br />
-      <button className=" border-2 mb-3" onClick={() => handleTimeIn24()}>in 24 hrs</button>
-      &nbsp;{is24hrs}
-    <br/>
-      <button className=" border-2 mb-3  " onClick={() => handleTimeDate()}>time-Date</button>
-      &nbsp;{isTimeDate}
-      <br />
-      <br />
+        <h2 className="text-xl font-medium mb-2">Date Formats</h2>
+        <button className=" border-2 mb-3  space-x-4" onClick={() => handleGetDay()}>format Date</button>
+
+        <p className=" space-x-4 text-pink-950">{isDay}</p>
+
+        <button className=" border-2 mb-3" onClick={() => handleFormatDate2()}>
+          format Date2 in ddMMyy
+        </button>
+        <p className=" space-x-4 text-pink-950">{isFormatDate2.toString()}</p>
+        <br />
+        <button className=" border-2 mb-3" onClick={() => handleddMMyy()}>
+          format Date3 in yyyyMMdd
+        </button>{" "}
+        <p className=" space-x-4 text-pink-950">{isFullYearFormat}</p>
+
+        <br />
+        <button className=" border-2 mb-3" onClick={() => handleDay()}>getday</button>
+        <p className=" space-x-4 text-pink-950">{inputDay}</p>
+
+        <br />
+        <button className=" border-2 mb-3" onClick={() => handleMonth()}>getMonth</button>
+        <p className=" space-x-4 text-pink-950">{isMonth.toString()}</p>
+        <br />
+        <button className=" border-2 mb-3" onClick={() => handleFormatdate()}>getYear</button>
+        <p className=" space-x-4 text-pink-950">{input}</p>
+        <br />
+        <button className=" border-2 mb-3" onClick={() => handleGetTime()}>get Time In HH:MM</button>
+        <p className=" space-x-4 text-pink-950">{isTime.toString()}</p>
+        <br />
+        <button className=" border-2 mb-3" onClick={() => handleFullTime()}>time in HH:MM:SS</button>
+        <p className=" space-x-4 text-pink-950">{isFullTime.toString()}</p>
+        <br />
+        <button className=" border-2 mb-3" onClick={() => handleFullMonth()}>get full month</button>
+        <p className=" space-x-4 text-pink-950">{isMonthFull.toString()}</p>
 
       </div>
 
       <div className="px-4">
-      <h3 className="text-xl  font-medium mb-2" >sorting</h3>
-     
-      <button className=" border-2 mb-3" onClick={() => handleAsc()}>Ascending</button>
-      &nbsp;
-      {/* {isAsc?.map((dates) => {
-        console.log(dates,"ASC dates")
-      })} */}
-      <br />
+        <h2 className="text-xl font-medium mb-2">Time Conversion</h2>
 
-      <button className=" border-2 mb-3" onClick={() => handleDesc()}>Descending</button>
-      &nbsp;
-      {/* {isDesc?.map((dates2, index: number) => {
-        console.log(dates2, "DESCdates");
-      })} */}
+        <button className=" border-2 mb-3"
+          onClick={() => {
+            handleTime();
+          }}
+        >
+          in 12 hrs
+        </button>
+        &nbsp;
+        <p className=" space-x-4 text-pink-950">{is12hrs.toString()}</p>
+        <br />
+        <button className=" border-2 mb-3" onClick={() => handleTimeIn24()}>in 24 hrs</button>
+        &nbsp;
+        <p className=" space-x-4 text-pink-950">{is24hrs.toString()}</p>
+        <br />
+        <button className=" border-2 mb-3  " onClick={() => handleTimeDate()}>time-Date</button>
+        &nbsp;
+        <p className=" space-x-4 text-pink-950">{isTimeDate.toString()}</p>
+        <br />
+        <br />
+
       </div>
+
+      <div className="px-4">
+        <h3 className="text-xl  font-medium mb-2" >Sorting</h3>
+
+        <button className=" border-2 mb-3" onClick={() => handleAsc()}>Ascending</button>
+        &nbsp;
+        <ul>
+          <li> 
+          <p className=" space-x-4 text-pink-950">{isAsc?.toString()} </p>
+          </li>
+        </ul>
+
+
+        <br />
+
+        <button className=" border-2 mb-3" onClick={() => handleDesc()}>Descending</button>
+        &nbsp;
+        <ul>
+          <li className=" space-x-4 text-pink-950"> 
+         { isDesc?.toString()  }
+           </li>
+        </ul>
+
+      </div>
+
+      
     </>
   );
 }
